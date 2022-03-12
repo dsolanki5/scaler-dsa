1881cfa337fe774ae52e5750b1d0038065fdf015
Deepak Solanki <dsdeepaksolanki8@gmail.com>
2022-03-12 23:00:58 +0530

    - solution to DSA interview by scaler Mentor - Deepak

--- src/MentorQuestions/FruitsBasket.java
+++ src/MentorQuestions/FruitsBasket.java
@@ -2,8 +2,36 @@ package MentorQuestions;
 
 public class FruitsBasket {
 
-	public static void main(String[] args) {
+	public static int getFruitBaskets(int A, int B, int C) {
+		int ans = 0;
+		int n = A+B+C;
 		
+		if(n<3 || A<1 || B<1)
+			return ans;
+		
+		for(int i=1; i<=n; i++) {
+			if(A==0 || B==0)
+				return ans;
+			
+			A--;
+			B--;
+			
+			if(C>0)
+				C--;
+			else if(B>0)
+				B--;
+			else
+				A--;
+			
+			ans++;
+		}
+		
+		return ans;
+	}
+	public static void main(String[] args) {
+//		int A = 3, B = 6, C=0;   //ans=3
+		int A = 10, B = 1, C=10;   //ans=1
+		System.out.println("Ans= "+FruitsBasket.getFruitBaskets(A, B, C));
 
 	}
 
